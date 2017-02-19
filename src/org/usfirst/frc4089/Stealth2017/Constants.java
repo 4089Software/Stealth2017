de3579187606dee51081f35e741e6fd6f235de4d
4089Software <stealthrobotics@gmail.com>
2017-02-19 14:25:44 -0800

    git hub is not letting me commit

--- src/org/usfirst/frc4089/Stealth2017/Constants.java
+++ src/org/usfirst/frc4089/Stealth2017/Constants.java
@@ -13,7 +13,7 @@ public class Constants {
 	public static final int left2MotorSpeedControl = 12;
 	public static final int climbMotor1SpeedControl = 3;
 	public static final int climbMotor2SpeedControl = 4;
-	public static final int ShooterMotorSpeedControl = 2;
-	public static final int elevatorMotorSpeedControl = 1;
+	public static final int ShooterMotorSpeedControl = 1;
+	public static final int elevatorMotorSpeedControl = 2;
 	public static final int collectorMotorControl = 5;
 }
